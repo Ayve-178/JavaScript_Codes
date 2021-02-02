394b6158869788bf96c74b87f4597dce2880aef8
Khairun Nessa Ayve <49992828+Ayve-178@users.noreply.github.com>
2021-02-03 01:04:29 +0600

    Create app1.js

---
+++ node JS/app1.js
@@ -0,0 +1,11 @@
+//After installing the latest version of node
+console.log("Hello World!");
+
+var a = 3
+var b = 2
+var c = a+b
+
+console.log("The output is "+ c);
+
+//We will open a terminal and write the command "node app1.js" and then hit enter. We will find the
+//output in the terminal.
