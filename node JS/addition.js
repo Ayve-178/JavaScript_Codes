4bce479ca0d8090c416048c8bc856e9f316677ba
Khairun Nessa Ayve <49992828+Ayve-178@users.noreply.github.com>
2021-02-03 01:05:24 +0600

    Update and rename app1.js to addition.js

--- node JS/addition.js
+++ node JS/addition.js
@@ -7,5 +7,5 @@ var c = a+b
 
 console.log("The output is "+ c);
 
-//We will open a terminal and write the command "node app1.js" and then hit enter. We will find the
+//We will open a terminal and write the command "node addition.js" and then hit enter. We will find the
 //output in the terminal.
